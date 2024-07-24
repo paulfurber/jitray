0f1d5504a2a107013e53b7acd9d29bde29c331cd
Paul Furber <paul.furber@gmail.com>
2024-07-24 16:39:19 +0200

    Indenting

--- examples/textures/textures_bunnymark.lua
+++ examples/textures/textures_bunnymark.lua
@@ -12,25 +12,25 @@ local Bunny = {}
 Bunny.__index = Bunny
 
 function Bunny:new(pos, spd, col)
-  local bunny = {}
-  setmetatable(bunny,Bunny)
-  bunny.position = pos
-  bunny.speed = spd
-  bunny.color = col
-  return bunny
+    local bunny = {}
+    setmetatable(bunny,Bunny)
+    bunny.position = pos
+    bunny.speed = spd
+    bunny.color = col
+    return bunny
 end
 
 function Bunny:update(texture)
-  self.position.x = self.position.x + self.speed.x
-  self.position.y = self.position.y + self.speed.y
-  if ((self.position.x + texture.width/2) > rl.GetScreenWidth())
-    or ((self.position.x + texture.width/2) < 0) then
-    self.speed.x = self.speed.x * -1
-  end
-  if ((self.position.y + texture.height/2) > rl.GetScreenHeight())
-    or ((self.position.y + texture.height/2 - 40) < 0) then
-    self.speed.y = self.speed.y * -1
-  end
+    self.position.x = self.position.x + self.speed.x
+    self.position.y = self.position.y + self.speed.y
+    if ((self.position.x + texture.width/2) > rl.GetScreenWidth())
+	or ((self.position.x + texture.width/2) < 0) then
+	self.speed.x = self.speed.x * -1
+    end
+    if ((self.position.y + texture.height/2) > rl.GetScreenHeight())
+	or ((self.position.y + texture.height/2 - 40) < 0) then
+	self.speed.y = self.speed.y * -1
+    end
 end
 
 -- Initialization
@@ -47,45 +47,45 @@ local bunnies = {}
 
 -- Main game loop
 while not rl.WindowShouldClose() do    -- Detect window close button or ESC key
-  -- Update
-  if rl.IsMouseButtonDown(rl.MOUSE_BUTTON_LEFT) then
-    -- Create more bunnies
-    for i = 1, 100 do
-      if #bunnies < MAX_BUNNIES then
-        local speed = rl.new("Vector2", rl.GetRandomValue(-250, 250) / 60, rl.GetRandomValue(-250, 250) / 60)
-        local color = rl.new("Color", rl.GetRandomValue(50, 240), rl.GetRandomValue(80, 240), rl.GetRandomValue(100, 240), 255)
-        table.insert(bunnies, Bunny:new(rl.GetMousePosition(), speed, color))
-      end
+    -- Update
+    if rl.IsMouseButtonDown(rl.MOUSE_BUTTON_LEFT) then
+	-- Create more bunnies
+	for i = 1, 100 do
+	    if #bunnies < MAX_BUNNIES then
+		local speed = rl.new("Vector2", rl.GetRandomValue(-250, 250) / 60, rl.GetRandomValue(-250, 250) / 60)
+		local color = rl.new("Color", rl.GetRandomValue(50, 240), rl.GetRandomValue(80, 240), rl.GetRandomValue(100, 240), 255)
+		table.insert(bunnies, Bunny:new(rl.GetMousePosition(), speed, color))
+	    end
+	end
     end
-  end
 
-  -- Update bunnies
-  for i=1,#bunnies do
-    bunnies[i]:update(texBunny)
-  end
+    -- Update bunnies
+    for i=1,#bunnies do
+	bunnies[i]:update(texBunny)
+    end
 
-  -- Draw
-  rl.BeginDrawing()
+    -- Draw
+    rl.BeginDrawing()
 
-  rl.ClearBackground(rl.RAYWHITE)
+    rl.ClearBackground(rl.RAYWHITE)
 
-  for i=1,#bunnies do
-    -- NOTE: When internal batch buffer limit is reached (MAX_BATCH_ELEMENTS),
-    -- a draw call is launched and buffer starts being filled again;
-    -- before issuing a draw call, updated vertex data from internal CPU buffer is send to GPU...
-    -- Process of sending data is costly and it could happen that GPU data has not been completely
-    -- processed for drawing while new data is tried to be sent (updating current in-use buffers)
-    -- it could generates a stall and consequently a frame drop, limiting the number of drawn bunnies
-    rl.DrawTexture(texBunny, bunnies[i].position.x, bunnies[i].position.y, bunnies[i].color);
-  end
+    for i=1,#bunnies do
+	-- NOTE: When internal batch buffer limit is reached (MAX_BATCH_ELEMENTS),
+	-- a draw call is launched and buffer starts being filled again;
+	-- before issuing a draw call, updated vertex data from internal CPU buffer is send to GPU...
+	-- Process of sending data is costly and it could happen that GPU data has not been completely
+	-- processed for drawing while new data is tried to be sent (updating current in-use buffers)
+	-- it could generates a stall and consequently a frame drop, limiting the number of drawn bunnies
+	rl.DrawTexture(texBunny, bunnies[i].position.x, bunnies[i].position.y, bunnies[i].color);
+    end
 
-  rl.DrawRectangle(0, 0, screenWidth, 40, rl.BLACK)
-  rl.DrawText("bunnies: " .. #bunnies, 120, 10, 20, rl.GREEN)
-  rl.DrawText("batched draw calls: " .. math.ceil(1 + #bunnies / MAX_BATCH_ELEMENTS), 320, 10, 20, rl.MAROON)
+    rl.DrawRectangle(0, 0, screenWidth, 40, rl.BLACK)
+    rl.DrawText("bunnies: " .. #bunnies, 120, 10, 20, rl.GREEN)
+    rl.DrawText("batched draw calls: " .. math.ceil(1 + #bunnies / MAX_BATCH_ELEMENTS), 320, 10, 20, rl.MAROON)
 
-  rl.DrawFPS(10, 10)
+    rl.DrawFPS(10, 10)
 
-  rl.EndDrawing()
+    rl.EndDrawing()
 end
 
 -- De-Initialization
